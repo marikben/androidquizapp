20e91e9a3b968c0d8424e89631e9854fde589491
marikben <70907667+marikben@users.noreply.github.com>
2021-01-04 22:10:35 +0200

    first commit

---
+++ app/src/main/java/com/example/inno3/StartScreen.java
@@ -0,0 +1,19 @@
+package com.example.inno3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aloitus);
+    }
+
+    //laita tähän start-mtodi
+
+
+}
